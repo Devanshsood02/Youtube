dacbf392d883bce7d27685f5eb5546060ec56d19
Devansh Sood <sooddevansh143@gmail.com>
2026-04-15 12:43:01 +0530

    15 april 2026

---
+++ Snitch/Backend/src/config/database.js
@@ -0,0 +1,15 @@
+
+import mongoose  from "mongoose";
+import { config } from "./config.js";
+
+
+export const connectToDB =async ()=>{
+
+    try{
+        await mongoose.connect(config.MONGO_URI)
+        console.log("Database is connected ")
+    }
+    catch(error){
+        console.log(error)
+    }
+}
